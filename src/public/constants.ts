f458b885ab4b3786858131543dc7836a4c65eb39
isma <xwarzmiixz@gmail.com>
2025-01-01 20:50:04 +0100

    ADD: Contenedores para base de datos, aplicación y logs

--- src/public/constants.ts
+++ src/public/constants.ts
@@ -1,4 +1,21 @@
-const { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, PORT, NODE_ENV } =
-  process.env;
+const {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  PORT,
+  NODE_ENV,
+  LOG_SERVER_URL,
+} = process.env;
 
-export { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, PORT, NODE_ENV };
+export {
+  DB_HOST,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_PORT,
+  PORT,
+  NODE_ENV,
+  LOG_SERVER_URL,
+};
--- src/public/logger.ts
+++ src/public/logger.ts
@@ -1,5 +1,6 @@
 import { createLogger, format, transports } from "winston";
-import { NODE_ENV } from "./constants";
+import LokiTransport from "winston-loki";
+import { LOG_SERVER_URL, NODE_ENV } from "./constants";
 
 const logger = createLogger({
   level: "info",
@@ -9,8 +10,22 @@ const logger = createLogger({
     format.splat(),
     format.json()
   ),
-  defaultMeta: { service: "user-service" },
+  defaultMeta: { service: "backend" },
   transports: [
+    new LokiTransport({
+      host: LOG_SERVER_URL || "http://logserver:3100",
+      labels: {
+        app: "backend",
+        service: "backend",
+        environment: NODE_ENV || "development",
+        job: "backend",
+      },
+      json: true,
+      replaceTimestamp: true,
+      level: "info",
+      onConnectionError: (err) =>
+        console.error("Error al conectar con Loki:", err),
+    }),
     new transports.File({ filename: "error.log", level: "error" }),
     new transports.File({ filename: "combined.log" }),
   ],
