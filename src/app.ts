92b5d6143de3b4fa008626f17990bd673a63c9ef
isma <xwarzmiixz@gmail.com>
2025-01-02 12:10:37 +0100

    DELETE: Eliminada variable irrelevante

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,12 @@
 import express from "express";
 import router from "./routes/router";
 import dotenv from "dotenv";
-import { PORT } from "@public/constants";
 
 dotenv.config();
 
 const app = express();
-const port = PORT ?? 3000;
 
 app.use(express.json());
 app.use("/", router);
 
-const server = app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port} port.`);
-});
-
-export { app, server };
+export { app };
