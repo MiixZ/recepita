705ae750dc9333974c573d273b61fb960b672cc1
isma <xwarzmiixz@gmail.com>
2025-01-18 14:26:59 +0100

    fix: formato port

--- src/app.ts
+++ src/app.ts
@@ -6,9 +6,13 @@ dotenv.config();
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use("/", router);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
 
 export { app };
